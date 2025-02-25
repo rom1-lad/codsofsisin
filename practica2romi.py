617337315eb2d78191d2e4e03943487f7e2fe746
rom1-lad <paramo.romina.2024@cbtis-246.edu.mx>
2025-02-24 18:12:15 -0600

    Add files via upload

---
+++ practica2romi.py
@@ -0,0 +1,23 @@
+#calcular en base a un valor dado por el usuario 
+#su equivalencia en las diferentes divisas definidas 
+
+yuan = 2.81	
+yen  = 0.13
+dólar = 20.54 
+euro = 21.56 
+libra = 25.56
+
+peso = input("ingresa la cantidad de peso a convertir//)
+
+peso = int(peso)
+
+print(peso/yuan
+
+print(
+
+
+
+
+
+
+ 
