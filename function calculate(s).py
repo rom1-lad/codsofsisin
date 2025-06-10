b435f9d0f45c01b8a941d197818b7088c41b00e3
rom1-lad <paramo.romina.2024@cbtis-246.edu.mx>
2025-06-10 16:01:07 -0600

    Add files via upload

---
+++ function calculate(s).py
@@ -0,0 +1,26 @@
+function calculate(s)
+
+oper = []
+for c in s:
+      if c == 'p':
+          oper.append('+')
+      if c == 'm':
+          oper.append('-')
+
+
+s = s.replace("plus",' ')
+s = s.replece("minus",' ')
+s = s.split(" ")
+
+
+result = int(s[0])
+     
+
+for i in range(len(oper)):
+      if oper[i] =='+':
+         result += int(s[i+1])
+      else:
+          result -= int(s[i+1])
+
+return str(result)
+         
